c5ac45880cf97d71721c7936a619b03260ec4666
David Brick <david@davidbrick.com>
2025-12-15 21:43:16 -0500

    added I am a developer

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
                 .scaledToFit()
                 .foregroundStyle(.orange)
             Text("You Are Awesome!")
+            Text("I am a developer!")
                 .font(.largeTitle)
                 .fontWeight(.thin)
                 .foregroundStyle(.green)
